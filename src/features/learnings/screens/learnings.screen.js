6c69557e84b42b8e9243b5daee31301fbca1a977
kenjic95 <kenjinescorpuz@gmail.com>
2026-03-25 21:22:13 +0900

    13.7 Category Cards Added

--- src/features/learnings/screens/learnings.screen.js
+++ src/features/learnings/screens/learnings.screen.js
@@ -13,6 +13,41 @@ const categories = [
     categoryPhoto:
       "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
   },
+  {
+    id: "personal-development-lifestyle",
+    categoryTitle: "Personal Development & Lifestyle",
+    noOfCourses: "20 Courses",
+    categoryPhoto:
+      "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
+  },
+  {
+    id: "creative-arts-media",
+    categoryTitle: "Creative Arts & Media",
+    noOfCourses: "16 Courses",
+    categoryPhoto:
+      "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
+  },
+  {
+    id: "home-hobbies",
+    categoryTitle: "Home & Hobbies",
+    noOfCourses: "17 Courses",
+    categoryPhoto:
+      "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
+  },
+  {
+    id: "language-education",
+    categoryTitle: "Language & Education",
+    noOfCourses: "10 Courses",
+    categoryPhoto:
+      "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
+  },
+  {
+    id: "finance-investing",
+    categoryTitle: "Finance & Investing",
+    noOfCourses: "13 Courses",
+    categoryPhoto:
+      "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
+  },
 ];
 
 export const LearningsScreen = () => (
