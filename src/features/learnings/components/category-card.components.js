8a0a4e45230f6516a937e0d2bbb41933652e2bfb
kenjic95 <kenjinescorpuz@gmail.com>
2026-03-25 20:44:44 +0900

    13.6 Styles and components were optimized

--- src/features/learnings/components/category-card.components.js
+++ src/features/learnings/components/category-card.components.js
@@ -1,45 +1,12 @@
 import React from "react";
-import styled from "styled-components/native";
-import { Card } from "react-native-paper";
 
-const CategoryCard = styled(Card)`
-  position: relative;
-  aspect-ratio: 1;
-  border-radius: 20px;
-  overflow: hidden;
-  background-color: ${(props) => props.theme.colors.ui.quaternary};
-`;
-
-const CategoryCardCover = styled(Card.Cover)`
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  top: ${(props) => props.theme.space[4]};
-  background-color: ${(props) => props.theme.colors.ui.quaternary};
-`;
-
-const Title = styled.Text`
-  margin-bottom: ${(props) => props.theme.space[0]};
-  color: ${(props) => props.theme.colors.text.primary};
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-weight: ${(props) => props.theme.fontWeights.bold};
-  font-size: ${(props) => props.theme.fontSizes.body};
-`;
-
-const CardOverlay = styled.View`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const CourseText = styled.Text`
-  color: ${(props) => props.theme.colors.text.disabled};
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.button};
-`;
+import {
+  CardOverlay,
+  CategoryCard,
+  CategoryCardCover,
+  CourseText,
+  Title,
+} from "./category-card.styles";
 
 export const CategoryInfo = ({ category } = {}) => {
   const {
@@ -49,9 +16,11 @@ export const CategoryInfo = ({ category } = {}) => {
       "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
     ],
   } = category ?? {};
+
   const coverPhoto = Array.isArray(categoryPhoto)
     ? categoryPhoto[0]
     : categoryPhoto;
+
   return (
     <CategoryCard elevation={5}>
       <CategoryCardCover source={{ uri: coverPhoto }} resizeMode="contain" />
---
+++ src/features/learnings/components/category-card.styles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components/native";
+import { Card } from "react-native-paper";
+
+export const CategoryCard = styled(Card)`
+  position: relative;
+  aspect-ratio: 1;
+  border-radius: 20px;
+  overflow: hidden;
+  background-color: ${(props) => props.theme.colors.ui.quaternary};
+`;
+
+export const CategoryCardCover = styled(Card.Cover)`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: ${(props) => props.theme.space[4]};
+  background-color: ${(props) => props.theme.colors.ui.quaternary};
+`;
+
+export const CardOverlay = styled.View`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const Title = styled.Text`
+  margin-bottom: ${(props) => props.theme.space[0]};
+  color: ${(props) => props.theme.colors.text.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-weight: ${(props) => props.theme.fontWeights.bold};
+  font-size: ${(props) => props.theme.fontSizes.body};
+`;
+
+export const CourseText = styled.Text`
+  color: ${(props) => props.theme.colors.text.disabled};
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.button};
+`;
