ab559f37b67e051999647310b1a0734c0362b673
kenjic95 <kenjinescorpuz@gmail.com>
2026-04-03 23:02:54 +1100

    16.3.2

--- src/services/learnings/categories.mock.js
+++ src/services/learnings/categories.mock.js
@@ -11,35 +11,34 @@ export const categoriesMock = [
     categoryTitle: "Personal Development & Lifestyle",
     noOfCourses: "20 Courses",
     categoryPhoto:
-      "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
+      "https://png.pngtree.com/png-vector/20221226/ourmid/pngtree-creativity-personal-development-individual-growth-concept-png-image_6538238.png",
   },
   {
     id: "creative-arts-media",
     categoryTitle: "Creative Arts & Media",
     noOfCourses: "16 Courses",
     categoryPhoto:
-      "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
+      "https://img.freepik.com/free-vector/paint-brushes-color-palette_1308-126704.jpg?semt=ais_incoming&w=740&q=80",
   },
   {
     id: "home-hobbies",
     categoryTitle: "Home & Hobbies",
     noOfCourses: "17 Courses",
-    categoryPhoto:
-      "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
+    categoryPhoto: "https://cdn-icons-png.flaticon.com/512/2853/2853386.png",
   },
   {
     id: "language-education",
     categoryTitle: "Language & Education",
     noOfCourses: "10 Courses",
     categoryPhoto:
-      "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
+      "https://www.stanthony.ca/wp-content/uploads/2020/06/40-409544_english-language-development-clipart-language-png-1.jpg",
   },
   {
     id: "finance-investing",
     categoryTitle: "Finance & Investing",
     noOfCourses: "13 Courses",
     categoryPhoto:
-      "https://media.istockphoto.com/id/1459130410/vector/healthy-kids.jpg?s=612x612&w=0&k=20&c=3nLz49a_U4bB_ob6HziTBbsiJTrqYdGxUlLytRASdZs=",
+      "https://c8.alamy.com/comp/W3FEPY/saving-money-business-finance-cartoon-W3FEPY.jpg",
   },
 ];
 
