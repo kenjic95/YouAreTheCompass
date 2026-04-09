6f53cbadd378647b61b1712fa33f12dae3828c48
Sanish Maharjan <maharjansanish204@gmail.com>
2026-04-09 22:16:06 +1000

    17.6.1 test

--- src/features/podcast/components/album-info-card.styles.js
+++ src/features/podcast/components/album-info-card.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components/native";
 import { Card } from "react-native-paper";
 
 export const AlbumCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.secondary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 export const AlbumCardCover = styled(Card.Cover)`
